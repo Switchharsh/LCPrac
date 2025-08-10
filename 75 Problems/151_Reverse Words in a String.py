208e43bbcb1ba3fe240030c7ca43620a376d70c2
Harsh Khandelwal <harshhkkhandelwal98@gmail.com>
2025-08-10 17:20:45 +0200

    151

---
+++ 75 Problems/151_Reverse Words in a String.py
@@ -0,0 +1,21 @@
+class Solution:
+    def reverseWords(self, s: str) -> str:
+        result = []
+        i = 0
+        n = len(s)
+        while i < n:
+            while i < n and s[i] == ' ':
+                i += 1
+            
+            if i >= n:
+                break
+
+            j = i+1
+            while j < n and s[j] != ' ' :
+                j+= 1
+
+            word = s[i:j]
+            result.append(word)
+
+            i = j+1
+        return ' '.join(reversed(result))
